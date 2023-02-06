be2afd9a7f3acdf8de9d322bac2223acc1517109
harshraj29 <rajh3282@gmail.com>
2023-02-07 02:39:30 +0530

    Home & Nav

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Frontend Devloper","Designer",
+    "Mern Stack Devloper"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
